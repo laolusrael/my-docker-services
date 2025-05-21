979c5749ac946e51ee4cf2ddfad886ed2d18f75b
Seun S. Lawal <seun.s.lawal@gmail.com>
2025-05-21 19:33:54 +0100

    initial commit

---
+++ azurite/deploy.cmd
@@ -0,0 +1,1 @@
+docker run --name "Azurite" -d --restart unless-stopped -p 10000:10000 -p 10001:10001 -p 10002:10002 -v ./data:/workspace  mcr.microsoft.com/azure-storage/azurite:latest azurite -l /workspace --blobHost 0.0.0.0 --loose --skipApiVersionCheck
---
+++ ollama/docker.ps1
@@ -0,0 +1,1 @@
+docker run -d --gpus=all -v models:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
